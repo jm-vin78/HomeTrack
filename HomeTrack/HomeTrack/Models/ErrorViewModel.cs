126ebe2b1a3d4fa83e95461b527b503661846984
Yulia Mishina <yulia.mishina78@gmail.com>
2020-08-05 15:11:54 +0300

    Empty template.

---
+++ HomeTrack/HomeTrack/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HomeTrack.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
