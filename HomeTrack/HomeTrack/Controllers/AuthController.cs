0a304f725356ba911b9c739d8f5dc4ed06b25151
Yulia Mishina <yulia.mishina78@gmail.com>
2020-08-27 20:10:58 +0300

    Added Registration.

--- HomeTrack/HomeTrack/Controllers/AuthController.cs
+++ HomeTrack/HomeTrack/Controllers/AuthController.cs
@@ -1,4 +1,6 @@
 ﻿using DAL;
+using DAL.Entities;
+using HomeTrack.Helpers;
 using HomeTrack.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
@@ -34,6 +36,7 @@ namespace HomeTrack.Controllers
             using (var context = factory.CreateContext())
             {
                 var IsUserValid = model.CheckUser(context, out var user);
+
                 if (!IsUserValid)
                     ModelState.AddModelError("form", "Неправильный логин и/или пароль.");
 
@@ -58,6 +61,43 @@ namespace HomeTrack.Controllers
             HttpContext.User = null;
             HttpContext.Session.Clear();
             Thread.CurrentPrincipal = null;
+
+            return RedirectToAction("Login");
+        }
+
+        public IActionResult Register()
+        {
+            return View(new RegisterModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register([FromServices] AppDbContextFactory factory, RegisterModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            using (var context = factory.CreateContext())
+            {
+                var phoneExists = model.CheckPhone(context);
+                if (phoneExists)
+                    ModelState.AddModelError("form", "Пользователь с таким номером телефона уже зарегистрирован.");
+
+                var emailExists = model.CheckEmail(context);
+                if (emailExists)
+                    ModelState.AddModelError("form", "Пользователь с таким электронным адресом уже зарегистрирован.");
+
+                if (!ModelState.IsValid)
+                    return View(model);
+
+                var entity = new UserEntity();
+                entity.Name = model.Name;
+                entity.Email = model.Email;
+                entity.Phone = model.Phone;
+                entity.PasswordHash = model.Password.ComputeHashMD5();
+
+                context.Users.Add(entity);
+                await context.SaveChangesAsync();
+            }
 
             return RedirectToAction("Login");
         }
---
+++ HomeTrack/HomeTrack/Models/RegisterModel.cs
@@ -0,0 +1,52 @@
+﻿using DAL;
+using DAL.Entities;
+using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Policy;
+using System.Threading.Tasks;
+
+namespace HomeTrack.Models
+{
+    public class RegisterModel
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Имя должно быть заполнено.")]
+        [Display(Name="Ваше имя *")]
+        [MaxLength(256)]
+        public string Name { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Телефон должен быть заполнен.")]
+        [Display(Name="Ваш телефон *")]
+        [MaxLength(10, ErrorMessage = "Максимальная длина значения 10.")]
+        public string Phone { get; set; }
+
+        [Display(Name="Адрес электронной почты")]
+        [MaxLength(256)]
+        public string Email { get; set; }
+        
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Необходимо ввести пароль.")]
+        [Display(Name="Пароль *")]
+        [MaxLength(20)]
+        public string Password { get; set; }
+
+        public bool CheckPhone(AppDbContext context)
+        {
+            var user = context.Users
+                .Where(u => u.Phone == Phone)
+                .SingleOrDefault();
+
+            return user != null;
+        }
+
+        public bool CheckEmail(AppDbContext context)
+        {
+            var user = context.Users
+                .Where(u => u.Email == Email)
+                .SingleOrDefault();
+
+            return user != null;
+        }
+    }
+}
